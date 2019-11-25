b0b3196151856f9172b6b7c5823336b31d9f536c
Shashi Kumar <shashi.raaz25@gmail.com>
2019-11-25 17:52:19 +0530

    model
    remove get and set

--- Module/src/main/java/com/shashi/model/Employee.java
+++ Module/src/main/java/com/shashi/model/Employee.java
@@ -9,42 +9,6 @@ private String number;
 private String addresh;
 private String password;
 private String conformpssword;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getNumber() {
-	return number;
-}
-public void setNumber(String number) {
-	this.number = number;
-}
-public String getAddresh() {
-	return addresh;
-}
-public void setAddresh(String addresh) {
-	this.addresh = addresh;
-}
-public String getPassword() {
-	return password;
-}
-public void setPassword(String password) {
-	this.password = password;
-}
-public String getConformpssword() {
-	return conformpssword;
-}
-public void setConformpssword(String conformpssword) {
-	this.conformpssword = conformpssword;
-}
 
 
 }
